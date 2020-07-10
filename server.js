0aa6c1dea2869b654f2a8de198b917c8630c386c
Flexxbo <flexxbo@gmail.com>
2020-07-11 00:00:27 +0200

    server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req,res)=>res.send("Hello WorldS"))
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on por ${PORT}`));
