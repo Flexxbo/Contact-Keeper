d37d3100fa82ea8cf260d584c72a2b97f9158f49
Flexxbo <flexxbo@gmail.com>
2020-07-11 18:07:53 +0200

    encrypted password and save userdata to db

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator/check"); //+here you can see what rules apply to the endpoints
 
 const User = require("../models/User");
 
@@ -10,20 +11,45 @@ const User = require("../models/User");
 router.post(
   "/",
   [
-    check("name", "Please add name").not().isEmpty(),
+    check("name", "Name is required").not().isEmpty(),
     check("email", "Please include a valid email").isEmail(),
     check(
       "password",
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("passed");
-    //res.send("request body kommt einfach nicht");
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      res.send("User saved");
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
   }
 );
 
--- server.js
+++ server.js
@@ -3,19 +3,17 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-// +++ Connect Database
+// Connect Database
 connectDB();
 
-// +++ Init Middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.json({ msg: "Welcome to Contact Keeper API" })); // Check with Postman if request returns response
-
-// +++ Define our routes
+// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
