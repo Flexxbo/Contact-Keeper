e355f023eb1d3833295dd380c3a6e1f13f8eafd3
Flexxbo <flexxbo@gmail.com>
2020-07-11 20:27:32 +0200

    added JWT Authentication and retrieving token

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check"); //+here you can see what rules apply to the endpoints
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../models/User");
 
@@ -44,8 +46,18 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      res.send("User saved");
 
+      const payload = { user: { id: user.id } };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("server error");
