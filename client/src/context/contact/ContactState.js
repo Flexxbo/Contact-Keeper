1fb7232ce39ad0148ab719a8b5811e11559678bd
Flexxbo <flexxbo@gmail.com>
2020-07-12 18:22:28 +0200

    pick contact ->current, then edit functionality

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,6 +37,7 @@ const ContactState = (props) => {
         type: "professional",
       },
     ],
+    current: null,
   };
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
@@ -51,9 +52,13 @@ const ContactState = (props) => {
     dispatch({ type: DELETE_CONTACT, payload: id });
   };
   //*SET_CURRENT
-
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
   //*CLEAR_CURRENT
-
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
   //*UPDATE_CONTACT
 
   //*FILTER_CONTACTS
@@ -62,7 +67,14 @@ const ContactState = (props) => {
 
   return (
     <ContactContext.Provider
-      value={{ contacts: state.contacts, addContact, deleteContact }}
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+      }}
     >
       {props.children}
     </ContactContext.Provider>
